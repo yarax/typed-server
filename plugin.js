485315f0cf3f38b31ebb165793ea10596e5b39a5
rax <raxpost@gmail.com>
2017-01-04 07:42:02 +0100

    plugin

--- plugin.js
+++ plugin.js
@@ -1,9 +1,36 @@
-module.exports = function ({types: t}) {
+const babylon = require('babylon');
+
+function analyzeType(typeNode, path, t, template) {
+  if (typeNode.id.name === 'Parameters') {
+    const params = [];
+    (typeNode.right.properties || []).forEach(prop => {
+      const propName = prop.key.name;
+      const extractor = prop.value.id.name
+      params.push(`${propName}: req.${extractor.toLowerCase()}.${propName}`);
+    });
+
+    const body = babylon.parse(`
+    module.exports.parameters = (req) => {
+      return {
+        ${params.join(',\n')}
+      };
+    };
+    `).program.body[0];
+    path.insertAfter(body);
+  }
+}
+
+module.exports = function (babel) {
+  console.log(Object.keys(babel));
+  const {types: t, template, parse} = babel;
   return {
     visitor: {
-      GenericTypeAnnotation(path, state) {
-        console.log(path.node.id && path.node.id.name);
-        path.replaceWithSourceString('ХУЙ');
+      ExportNamedDeclaration(path, state) {
+        const node = path.node;
+        if (node.declaration.type === 'TypeAlias') {
+          analyzeType(node.declaration, path, t, template);
+        }
+        //path.replaceWithSourceString('ХУЙ');
       },
     }
   };
